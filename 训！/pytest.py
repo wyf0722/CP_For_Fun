54d889204e79baf0ffb1cdcae37898ccbd666348
wyf0722 <522022230116@smail.nju.edu.cn>
2024-06-12 17:17:48 +0800

    添加pytest.py

---
+++ 训！/pytest.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import accumulate, count, pairwise, permutations
+from math import comb, gcd, inf, lcm, log2, perm
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+from typing import List
+from sortedcontainers import SortedDict, SortedList, SortedSet
+"""
+stuff you should look for
+int overflow, array bounds
+special cases (n=1?)
+do smth instead of nothing and stay organized
+WRITE STUFF DOWN
+DON'T GET STUCK ON ONE APPROACH
+"""
+"""
+testcases = [
+    [[2,3,0,0,2], 4, 'd'],
+]
+s = Solution()
+func_name = dir(s)[-1]
+func = getattr(s, func_name)
+for args in testcases:
+    print(func(*args))
+"""
+LOCAL_DEBUG = 1
+if LOCAL_DEBUG:
+    def debug(*args, **kwargs):
+        print("\033[35m", end="")
+        print(*args, **kwargs)
+        print("\033[0m", end="")
+
+# 𝓽𝓱𝓮 𝓼𝓸𝓵𝓾𝓽𝓲𝓸𝓷 𝓸𝓯 𝔀𝔂𝓯0722
