8fea96dafcbde91ab9a4535f1ce596fd535f478e
wyf0722 <522022230116@smail.nju.edu.cn>
2024-06-16 21:27:12 +0800

    更新gym.py debug函数

--- Gym/gym.py
+++ Gym/gym.py
@@ -1,3 +1,4 @@
+import inspect
 import sys
 from bisect import bisect_left, bisect_right
 from collections import Counter, defaultdict, deque
@@ -17,6 +18,16 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 
+DEBUG_WITH_COLOR = 1
 def debug(*args, **kwargs):
-    print("Line " + str(sys._getframe().f_lineno), end=": ")
-    print(*args, **kwargs)
+    stack_info = inspect.stack()[1]  # [1] 表示获取调用栈中的第二个帧（第一个帧是当前函数）
+    line_number = stack_info.lineno
+    # 定义ANSI颜色代码
+    COLOR = '\033[95m'
+    RESET = '\033[0m'
+    # 打印调试信息
+    if DEBUG_WITH_COLOR:
+        print(f"{COLOR}Line {line_number}{RESET}: ", *args, **kwargs)
+    else:
+        print(f"Line {line_number}: ", *args, **kwargs)
+
