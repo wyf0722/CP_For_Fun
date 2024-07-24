bbb5b518fb34f5b79defe335fcb02b648054c785
wyf0722 <522022230116@smail.nju.edu.cn>
2024-07-24 23:40:15 +0800

    fixed: update macros for cpp

--- Arena/Fan.cpp
+++ Arena/Fan.cpp
@@ -13,9 +13,7 @@ using namespace std;
 #define rall(x) begin(x), end(x)
 // loops
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define F0R(i, a) FOR(i, 0, a)
 #define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i, a) ROF(i, 0, a)
 #define rep(a) F0R(_, a)
 #define each(a, x) for (auto &a : x)
 using i64 = long long;
@@ -35,70 +33,22 @@ int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
 template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
 template<typename T> using max_heap=priority_queue<T>;
 
-template <class T>
-void mkuni(vector<T>& v) {
-    sort(all(v));
-    v.erase(unique(all(v)), v.end());
-}
-
-template <class T>
-int lwb(vector<T>& a, const T& b) {
-    return int(lower_bound(all(a), b) - begin(a));
-}
-
-template <class T>
-int upb(vector<T>& a, const T& b) {
-    return int(upper_bound(all(a), b) - begin(a));
-}
-
-constexpr int popcount(int x) {
-    return __builtin_popcount(x);
-}
-
-constexpr int topbit(int x) {
-    return x == 0 ? 0 : 31 - __builtin_clz(x);
-}
-
-template <class T, class U>
-void safeErase(T& t, const U& u) {
-    auto it = t.find(u);
-    assert(it != end(t));
-    t.erase(it);
-}
-
-template<class T, class U>
-T fstTrue(T lo, T hi, U f) {
-    ++hi;
-    assert(lo <= hi);  // assuming f is increasing
-    while (lo < hi) {  // find first index such that f is true
-        T mid = lo + (hi - lo) / 2;
-        f(mid) ? hi = mid : lo = mid + 1;
-    }
-    return lo;
-}
-
-template<class T, class U>
-T lstTrue(T lo, T hi, U f) {
-    --lo;
-    assert(lo <= hi);  // assuming f is decreasing
-    while (lo < hi) {  // find first index such that f is true
-        T mid = lo + (hi - lo + 1) / 2;
-        f(mid) ? lo = mid : hi = mid - 1;
-    }
-    return lo;
-}
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
 
 inline void add_mod(int& x, int y) { x += y; if (x >= MOD) x -= MOD; }
 inline void sub_mod(int& x, int y) { x += MOD - y; if (x >= MOD) x -= MOD; }
 
-template <class T>
-bool chmin(T& a, const T& b) {
-    return b < a ? a = b, 1 : 0;
-}  // set a = min(a,b)
-template <class T>
-bool chmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0;
-}  // set a = max(a,b)
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template <class T> void mkuni(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <class T> int lwb(vector<T>& a, const T& b) { return int(lower_bound(all(a), b) - begin(a)); }
+template <class T> int upb(vector<T>& a, const T& b) { return int(upper_bound(all(a), b) - begin(a)); }
+template <class T, class U> void safeErase(T& t, const U& u) { auto it = t.find(u); assert(it != end(t)); t.erase(it); }
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
 
 #ifdef FAN_DEBUG
 #include "../Util_For_Debug/template_cpp20_std.cpp"
--- Gym/gym.cpp
+++ Gym/gym.cpp
@@ -4,18 +4,16 @@ using namespace std;
 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
- 
+
 void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-#define debug(...) cout << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 
 #define all(x) begin(x), end(x)
 #define rall(x) begin(x), end(x)
 // loops
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define F0R(i, a) FOR(i, 0, a)
 #define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i, a) ROF(i, 0, a)
 #define rep(a) F0R(_, a)
 #define each(a, x) for (auto &a : x)
 using i64 = long long;
@@ -35,72 +33,23 @@ int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
 template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
 template<typename T> using max_heap=priority_queue<T>;
 
-template <class T>
-void mkuni(vector<T>& v) {
-    sort(all(v));
-    v.erase(unique(all(v)), v.end());
-}
-
-template <class T>
-int lwb(vector<T>& a, const T& b) {
-    return int(lower_bound(all(a), b) - begin(a));
-}
-
-template <class T>
-int upb(vector<T>& a, const T& b) {
-    return int(upper_bound(all(a), b) - begin(a));
-}
-
-constexpr int popcount(int x) {
-    return __builtin_popcount(x);
-}
-
-constexpr int topbit(int x) {
-    return x == 0 ? 0 : 31 - __builtin_clz(x);
-}
-
-template <class T, class U>
-void safeErase(T& t, const U& u) {
-    auto it = t.find(u);
-    assert(it != end(t));
-    t.erase(it);
-}
-
-template<class T, class U>
-T fstTrue(T lo, T hi, U f) {
-	++hi;
-	assert(lo <= hi);  // assuming f is increasing
-	while (lo < hi) {  // find first index such that f is true
-		T mid = lo + (hi - lo) / 2;
-		f(mid) ? hi = mid : lo = mid + 1;
-	}
-	return lo;
-}
-
-template<class T, class U>
-T lstTrue(T lo, T hi, U f) {
-	--lo;
-	assert(lo <= hi);  // assuming f is decreasing
-	while (lo < hi) {  // find first index such that f is true
-		T mid = lo + (hi - lo + 1) / 2;
-		f(mid) ? lo = mid : hi = mid - 1;
-	}
-	return lo;
-}
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
 
 inline void add_mod(int& x, int y) { x += y; if (x >= MOD) x -= MOD; }
 inline void sub_mod(int& x, int y) { x += MOD - y; if (x >= MOD) x -= MOD; }
 
-template <class T>
-bool chmin(T& a, const T& b) {
-    return b < a ? a = b, 1 : 0;
-}  // set a = min(a,b)
-template <class T>
-bool chmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0;
-}  // set a = max(a,b)
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template <class T> void mkuni(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <class T> int lwb(vector<T>& a, const T& b) { return int(lower_bound(all(a), b) - begin(a)); }
+template <class T> int upb(vector<T>& a, const T& b) { return int(upper_bound(all(a), b) - begin(a)); }
+template <class T, class U> void safeErase(T& t, const U& u) { auto it = t.find(u); assert(it != end(t)); t.erase(it); }
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
 
 /***
  * 𝓽𝓱𝓮 𝓼𝓸𝓵𝓾𝓽𝓲𝓸𝓷 𝓸𝓯 𝔀𝔂𝓯0722
  */
-
