6e4dd49ded2ede2a1c30199a08086ab37b7d2aba
wyf0722 <522022230116@smail.nju.edu.cn>
2024-10-29 00:52:21 +0800

    feat(CP_leetcode): biweekly 142 D

---
+++ CP_leetcode/biweekly/142/D.cpp
@@ -0,0 +1,380 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define all(x) begin(x), end(x)
+#define rall(x) begin(x), end(x)
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+using i64 = long long;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<i64, i64>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 1000000007; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+inline void add_mod(int& x, int y) { x += y; if (x >= MOD) x -= MOD; }
+inline void sub_mod(int& x, int y) { x += MOD - y; if (x >= MOD) x -= MOD; }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template <class T> void mkuni(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <class T> int lwb(vector<T>& a, const T& b) { return int(lower_bound(all(a), b) - begin(a)); }
+template <class T> int upb(vector<T>& a, const T& b) { return int(upper_bound(all(a), b) - begin(a)); }
+template <class T, class U> void safeErase(T& t, const U& u) { auto it = t.find(u); assert(it != end(t)); t.erase(it); }
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+/***
+ * 𝓽𝓱𝓮 𝓼𝓸𝓵𝓾𝓽𝓲𝓸𝓷 𝓸𝓯 𝔀𝔂𝓯0722
+ */
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using u128 = unsigned __int128;
+
+template<class T>
+constexpr T power(T a, u64 b, T res = 1) {
+    for (; b != 0; b /= 2, a *= a) {
+        if (b & 1) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+template<u32 P>
+constexpr u32 mulMod(u32 a, u32 b) {
+    return u64(a) * b % P;
+}
+
+template<u64 P>
+constexpr u64 mulMod(u64 a, u64 b) {
+    u64 res = a * b - u64(1.L * a * b / P - 0.5L) * P;
+    res %= P;
+    return res;
+}
+
+constexpr i64 safeMod(i64 x, i64 m) {
+    x %= m;
+    if (x < 0) {
+        x += m;
+    }
+    return x;
+}
+
+constexpr std::pair<i64, i64> invGcd(i64 a, i64 b) {
+    a = safeMod(a, b);
+    if (a == 0) {
+        return {b, 0};
+    }
+    
+    i64 s = b, t = a;
+    i64 m0 = 0, m1 = 1;
+
+    while (t) {
+        i64 u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;
+        
+        std::swap(s, t);
+        std::swap(m0, m1);
+    }
+    
+    if (m0 < 0) {
+        m0 += b / s;
+    }
+    
+    return {s, m0};
+}
+
+template<std::unsigned_integral U, U P>
+struct ModIntBase {
+public:
+    constexpr ModIntBase() : x(0) {}
+    template<std::unsigned_integral T>
+    constexpr ModIntBase(T x_) : x(x_ % mod()) {}
+    template<std::signed_integral T>
+    constexpr ModIntBase(T x_) {
+        using S = std::make_signed_t<U>;
+        S v = x_;
+        v %= S(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+    
+    constexpr static U mod() {
+        return P;
+    }
+    
+    constexpr U val() const {
+        return x;
+    }
+    
+    constexpr ModIntBase operator-() const {
+        ModIntBase res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+    
+    constexpr ModIntBase inv() const {
+        return power(*this, mod() - 2);
+    }
+    
+    constexpr ModIntBase &operator*=(const ModIntBase &rhs) & {
+        x = mulMod<mod()>(x, rhs.val());
+        return *this;
+    }
+    constexpr ModIntBase &operator+=(const ModIntBase &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator-=(const ModIntBase &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator/=(const ModIntBase &rhs) & {
+        return *this *= rhs.inv();
+    }
+    
+    friend constexpr ModIntBase operator*(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator+(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator-(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator/(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+    
+    friend constexpr std::istream &operator>>(std::istream &is, ModIntBase &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const ModIntBase &a) {
+        return os << a.val();
+    }
+    
+    friend constexpr std::strong_ordering operator<=>(ModIntBase lhs, ModIntBase rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+    
+private:
+    U x;
+};
+
+template<u32 P>
+using ModInt = ModIntBase<u32, P>;
+template<u64 P>
+using ModInt64 = ModIntBase<u64, P>;
+
+struct Barrett {
+public:
+    Barrett(u32 m_) : m(m_), im((u64)(-1) / m_ + 1) {}
+
+    constexpr u32 mod() const {
+        return m;
+    }
+
+    constexpr u32 mul(u32 a, u32 b) const {
+        u64 z = a;
+        z *= b;
+        
+        u64 x = u64((u128(z) * im) >> 64);
+        
+        u32 v = u32(z - x * m);
+        if (m <= v) {
+            v += m;
+        }
+        return v;
+    }
+
+private:
+    u32 m;
+    u64 im;
+};
+
+template<u32 Id>
+struct DynModInt {
+public:
+    constexpr DynModInt() : x(0) {}
+    template<std::unsigned_integral T>
+    constexpr DynModInt(T x_) : x(x_ % mod()) {}
+    template<std::signed_integral T>
+    constexpr DynModInt(T x_) {
+        int v = x_;
+        v %= int(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+    
+    constexpr static void setMod(u32 m) {
+        bt = m;
+    }
+    
+    static u32 mod() {
+        return bt.mod();
+    }
+    
+    constexpr u32 val() const {
+        return x;
+    }
+    
+    constexpr DynModInt operator-() const {
+        DynModInt res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+    
+    constexpr DynModInt inv() const {
+        auto v = invGcd(x, mod());
+        assert(v.first == 1);
+        return v.second;
+    }
+    
+    constexpr DynModInt &operator*=(const DynModInt &rhs) & {
+        x = bt.mul(x, rhs.val());
+        return *this;
+    }
+    constexpr DynModInt &operator+=(const DynModInt &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator-=(const DynModInt &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator/=(const DynModInt &rhs) & {
+        return *this *= rhs.inv();
+    }
+    
+    friend constexpr DynModInt operator*(DynModInt lhs, const DynModInt &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator+(DynModInt lhs, const DynModInt &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator-(DynModInt lhs, const DynModInt &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator/(DynModInt lhs, const DynModInt &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+    
+    friend constexpr std::istream &operator>>(std::istream &is, DynModInt &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const DynModInt &a) {
+        return os << a.val();
+    }
+    
+    friend constexpr std::strong_ordering operator<=>(DynModInt lhs, DynModInt rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+    
+private:
+    u32 x;
+    static Barrett bt;
+};
+
+template<u32 Id>
+Barrett DynModInt<Id>::bt = 998244353;
+
+using Z = ModInt<1000000007>;
+
+class Solution {
+public:
+    int possibleStringCount(string word, int k) {
+        int n = word.size();
+        if (n < k) return 0;
+        Z ans = 1;
+        vector<int> cnts;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt++;
+            if (i == n - 1 || word[i] != word[i + 1]) {
+                cnts.push_back(cnt);
+                ans *= cnt;
+                cnt = 0;
+            }
+        }
+        int m = cnts.size();
+        if (m >= k) return ans.val();
+        
+        // f[i][j]前i组构造长度为j的初始字符串的方案数目
+        vector<vector<Z>> f(m + 1, vector<Z>(k));
+        f[0][0] = 1;
+        vector<Z> s(k + 1);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) {
+                s[j + 1] = s[j] + f[i][j];
+            }
+            // f[i + 1][j] = f[i][j - 1] + f[i][j - 2] + ... + f[i][j - cnt[i]]
+            // [max(0, j - cnt[i]), j - 1]
+            for (int j = i + 1; j < k; j++) {
+                f[i + 1][j] = s[j] - s[max(0, j - cnts[i])];
+            }
+        }
+        for (int j = m; j < k; j++) {
+            ans -= f.back()[j];
+        }
+        return ans.val();
+    }
+};
