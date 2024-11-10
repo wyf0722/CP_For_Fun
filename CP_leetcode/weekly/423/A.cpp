3f9d8434ec96264b213bf597db799cdde4bf7b87
wyf0722 <522022230116@smail.nju.edu.cn>
2024-11-10 14:35:52 +0800

    feat(CP_leetcode): weekly423 ABC

---
+++ CP_leetcode/weekly/423/A.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2024-11-10 14:08
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> c(n);
+        int cnt = 1;
+        for (int i = 0; i < n; i++) {
+            if (i && nums[i - 1] < nums[i]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            c[i] = cnt;
+        }
+        for (int i = 0; i < n; i++) {
+            if (c[i] >= k && i + k < n && c[i + k] >= k) return true;
+        }
+        return false;
+    }
+};
