19d9dcd33626a5eee9183d411ebbf7358d2ac540
wyf0722 <522022230116@smail.nju.edu.cn>
2024-11-10 15:14:15 +0800

    feat(CP_leetcode): weekly423 D

---
+++ CP_leetcode/weekly/423/D.cpp
@@ -0,0 +1,102 @@
+/*************************************************************************
+    > File Name: D.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2024-11-10 15:11
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define all(x) begin(x), end(x)
+#define rall(x) begin(x), end(x)
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+using i64 = long long;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<i64, i64>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+int MOD = 1e9 + 7;
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+inline void add_mod(int& x, int y) { x += y; if (x >= MOD) x -= MOD; }
+inline void sub_mod(int& x, int y) { x += MOD - y; if (x >= MOD) x -= MOD; }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template <class T> void mkuni(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+template <class T> int lwb(vector<T>& a, const T& b) { return int(lower_bound(all(a), b) - begin(a)); }
+template <class T> int upb(vector<T>& a, const T& b) { return int(upper_bound(all(a), b) - begin(a)); }
+template <class T, class U> void safeErase(T& t, const U& u) { auto it = t.find(u); assert(it != end(t)); t.erase(it); }
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+/**
+ * f(x)表示x的二进制下1的个数
+ * g(x)表示 f(f(...f(x))) = 1的最小嵌套次数
+ * g(x) = g(f(x)) + 1
+ */
+class Solution {
+public:
+    int countKReducibleNumbers(string s, int k) {
+        int ans = 0;
+        int n = s.size();
+        vector<vector<int>> mem(n, vector<int>(n + 1, -1));
+        auto dfs = [&](auto &&self, int i, int one, bool is_limit) -> int {
+            if (i == n) {
+                return !is_limit && one == 0 ? 1 : 0;
+            }
+            if (!is_limit && mem.at(i).at(one) != -1) {
+                return mem.at(i).at(one);
+            }
+            int res = 0;
+            int up = is_limit ? s.at(i) - '0' : 1;
+            // fill 0
+            res += self(self, i + 1, one, is_limit && 0 == up);
+            res %= MOD;
+            // fill 1
+            if (one && up) {
+                res += self(self, i + 1, one - 1, is_limit && 1 == up);
+                res %= MOD;
+            }
+            if (!is_limit) mem.at(i).at(one) = res;
+            return res;
+        };
+
+        vector<int> f(n + 1);
+        for (int i = 1; i <= n; i++) {
+            f.at(i) = f.at(__builtin_popcount(i)) + 1;
+            if (f[i] <= k) {
+                ans += dfs(dfs, 0, i, true);
+                ans %= MOD;
+            }
+        }
+        return ans;
+    }
+};
