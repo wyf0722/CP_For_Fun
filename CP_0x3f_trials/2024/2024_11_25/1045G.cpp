5b2c994e4ebf14c87a29be9818c179c8dccc7a6e
wyf0722 <522022230116@smail.nju.edu.cn>
2024-11-29 16:54:19 +0800

    feat(CP_0x3f_trials): 2024_11_29 1045G submit with C++20 (GCC 13-64) notice that line 133: if (g.find(iq) == g.end()) continue; add this to avoid TLE

---
+++ CP_0x3f_trials/2024/2024_11_25/1045G.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+// void dbg_out() { cout << endl; }
+// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+// #define debug(...) cout << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+#ifdef FAN_DEBUG
+#include "../Util_For_Debug/template_cpp20_std.cpp"
+#else
+#define debug(...)
+#endif
+
+/***
+ *
+ *   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ *  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ *  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ *  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ *           ░     ░ ░      ░  ░
+ */
+#define MULTICASE 0
+template <typename T>
+struct Fenwick {
+    int n;
+    std::vector<T> a;
+    
+    Fenwick(int n_ = 0) {
+        init(n_);
+    }
+    
+    void init(int n_) {
+        n = n_;
+        a.assign(n, T{});
+    }
+    
+    void add(int x, const T &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] = a[i - 1] + v;
+        }
+    }
+    
+    T sum(int x) {
+        T ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            ans = ans + a[i - 1];
+        }
+        return ans;
+    }
+    
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+    
+    int select(const T &k) {
+        int x = 0;
+        T cur{};
+        for (int i = 1 << std::__lg(n); i; i /= 2) {
+            if (x + i <= n && cur + a[x + i - 1] <= k) {
+                x += i;
+                cur = cur + a[x - 1];
+            }
+        }
+        return x;
+    }
+};
+
+// don't forget to set default value
+// T variable = value
+struct Info{
+    int x;
+    Info(int x_ = 0):x(x_) {}
+};
+Info operator+(const Info &a, const Info &b) {
+    return Info{max(a.x, b.x)};
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> robots;
+    unordered_map<int, vector<int>> g;
+    for (int i = 0; i < n; i++) {
+        int x, r, q;
+        cin >> x >> r >> q;
+        robots.push_back({x, r, q});
+        g[q].push_back(x);
+    }
+
+    // 根据半径降序排列
+    sort(robots.begin(), robots.end(), [&](auto &e1, auto &e2) {
+        return e1[1] > e2[1];
+    });
+
+    unordered_map<int, Fenwick<int>> fens;
+    for (auto &[q, xs] : g) {
+        sort(xs.begin(), xs.end());
+        fens[q] = Fenwick<int>(xs.size());
+    }
+
+    i64 ans = 0;
+    for (auto &robot : robots) {
+        int x = robot[0], r = robot[1], q = robot[2];
+        // k范围小，枚举iq
+        for (int iq = q - k; iq <= q + k; iq++) {
+            // 位置[x - r, x + r]
+            if (g.find(iq) == g.end()) continue;
+            int R = upper_bound(g[iq].begin(), g[iq].end(), x + r) - g[iq].begin();
+            int L = lower_bound(g[iq].begin(), g[iq].end(), x - r) - g[iq].begin();
+            ans += fens[iq].rangeSum(L, R);
+        }
+        int pos = lower_bound(g[q].begin(), g[q].end(), x) - g[q].begin();
+        fens[q].add(pos, 1);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
