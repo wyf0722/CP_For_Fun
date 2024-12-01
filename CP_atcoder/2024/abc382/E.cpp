923c17337ad22ce7b8c6b4f9ea41468bebd4f203
wyf0722 <522022230116@smail.nju.edu.cn>
2024-12-01 18:45:32 +0800

    feat(CP_atcoder): abc382 E

---
+++ CP_atcoder/2024/abc382/E.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+// void dbg_out() { cout << endl; }
+// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+// #define debug(...) cout << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+#ifdef FAN_DEBUG
+#include "../Util_For_Debug/template_cpp20_std.cpp"
+#else
+#define debug(...)
+#endif
+
+/***
+ *
+ *   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ *  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ *  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ *  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ *           ░     ░ ░      ░  ░
+ */
+#define MULTICASE 0
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> p(n);
+    for (int &x : p) {
+        cin >> x;
+    }
+
+    // dp[i][j] = sum(dp[i - 1][j] * pp + dp[i - 1][j - 1] * (1 - pp))
+    vector<double> dp{1};
+    for (int i = 0; i < n; i++) {
+        double pp = p[i] * 0.01;
+        dp.push_back(0);
+        for (int j = i; j >= 0; j--) {
+            dp[j + 1] += dp[j] * pp;
+            dp[j] *= 1 - pp;
+        }
+    }
+    
+    double s = 1 - dp[0];
+    for (int i = 1; i <= n; i++) {
+        dp[i] /= s;
+    }
+
+    // f[i] = 1 + sum(f[i - j] * dp[j] | 0 <= j <= n)
+    // f[i](1 - dp[0]) = 1 + sum(f[i - j] * dp[j] | 1 <= j <= n);
+    vector<double> f(x + 1);
+    for (int i = 1; i <= x; i++) {
+        for (int j = 1; j <= n; j++) {
+            f[i] += f[max(i - j, 0)] * dp[j];
+        }
+        f[i] += 1 / s;
+    }
+    cout << f[x] << endl;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
