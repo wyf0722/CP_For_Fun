d0d1ea60142402194d8da87e64d668fe1b43264c
wyf0722 <522022230116@smail.nju.edu.cn>
2024-12-09 12:35:07 +0800

    feat(CP_0x3f_trials): 2024_12_09 1980D

---
+++ CP_0x3f_trials/2024/2024_12_09/1980D.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+// void dbg_out() { cout << endl; }
+// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+// #define debug(...) cout << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+#ifdef FAN_DEBUG
+#include "../Util_For_Debug/template_cpp20_std.cpp"
+#else
+#define debug(...)
+#endif
+
+/***
+ *
+ *   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ *  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ *  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ *  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ *           ░     ░ ░      ░  ░
+ */
+#define MULTICASE 1
+void solve() {
+    int n;
+    cin >> n;
+    V<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    V<int> pre(n), suf(n);
+    pre[0] = -inf;
+    suf[n - 1] = inf;
+    for (int i = 1; i < n; i++) {
+        if (pre[i - 1] == -1) pre[i] = -1;
+        else {
+            int g = gcd(a[i], a[i - 1]);
+            if (g < pre[i - 1]) pre[i] = -1;
+            else pre[i] = g;
+        }
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        if (suf[i + 1] == -1) suf[i] = -1;
+        else {
+            int g = gcd(a[i], a[i + 1]);
+            if (suf[i + 1] != 0 && g > suf[i + 1]) suf[i] = -1;
+            else suf[i] = g;
+        }
+    }
+    bool ok = false;
+    for (int i = 0; i < n; i++) {
+        bool left = (i == 0 || pre[i - 1] != -1);
+        bool right = (i == n - 1 || suf[i + 1] != -1);
+        if (!left || !right) continue;
+        if (i == 0 || i == n - 1) ok = true;
+        int g = gcd(a[i - 1], a[i + 1]);
+        if (pre[i - 1] <= g && g <= suf[i + 1]) ok = true;
+    }
+    cout << (ok ? "YES" : "NO") << endl;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
