3274efe55c966a4b58052e6da7ef712a4cf2a42e
wyf0722 <522022230116@smail.nju.edu.cn>
2024-12-22 23:09:39 +0800

    feat(CP_leetcode): weekly 429 CD

---
+++ CP_leetcode/weekly/429/C&D.cpp
@@ -0,0 +1,72 @@
+/*************************************************************************
+    > File Name: C.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2024-12-22 23:00
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+class Solution {
+public:
+    int minLength(string s, int numOps) {
+        int n = s.size();
+        // 特判len = 1的情况
+        auto f = [&](int head) -> bool {
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                int cur = s[i] - '0';
+                if (cur != head) cnt++;
+                head ^= 1;
+            }
+            return cnt <= numOps;
+        };
+        if (f(0) || f(1)) return 1;
+
+        auto ck = [&](int x) -> bool {
+            int cnt = 0;
+            for (int i = 0; i < n;) {
+                int st = i;
+                while (i < n && s[i] == s[st]) i++;
+                int len = i - st;
+                cnt += len / (x + 1);
+            }
+            return cnt <= numOps;
+        };
+
+        int lo = 1, hi = n;
+        while (lo + 1 < hi) {
+            int mid = (lo + hi) / 2;
+            if (ck(mid)) hi = mid;
+            else lo = mid;
+        }
+        return hi;
+    }
+};
