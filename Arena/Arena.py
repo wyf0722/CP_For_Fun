92a69ecb15308957c9787fb46a9e8981dbc7d074
wyf0722 <522022230116@smail.nju.edu.cn>
2025-03-16 23:42:06 +0800

    feat: update python io template

--- Arena/Arena.py
+++ Arena/Arena.py
@@ -1,70 +1,180 @@
-import os
-import random
-import sys
-from typing import *
-from collections import defaultdict, Counter, deque
-from functools import cache, reduce
-from itertools import pairwise, combinations, permutations, groupby, accumulate
-from bisect import bisect_left, bisect_right, insort_left, insort_right
-from heapq import *
-from math import gcd, lcm, isqrt
-from operator import add, sub, mul, floordiv, truediv, and_, or_, xor
+standard_input, packages, output_together = 1, 1, 0
+dfs, hashing, read_from_file = 0, 0, 0
+de = 1
 
-from types import GeneratorType
+if 1:
 
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
 
-def bootstrap(f, stack=[]):
+        import math
+        inf = math.inf
 
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
+        def I():
+            return input()
+
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return input().split()
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
                         break
-                    to = stack[-1].send(to)
-            return to
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
 
-    return wrappedfunc
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
 
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
 
-input = sys.stdin.readline
-output = lambda x: sys.stdout.write(str(x) + "\n")
-outputL = lambda x: sys.stdout.write(" ".join(map(str, x)) + "\n")
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
 
-printerr = lambda *args, **kwargs: print(
-    "\u001B[31m", *args, "\u001B[0m", file=sys.stderr, **kwargs)
+        sys.stdout = IOWrapper(sys.stdout)
 
-I = lambda: input().rstrip("\n")
-II = lambda: int(input())
-MII = lambda: map(int, input().split())
-LMII = lambda: list(MII())
-TMII = lambda: tuple(MII())
-LI = lambda: list(I())
-LSI = lambda: list(map(int, I()))
-#
-#   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
-# ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
-# ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
-# ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
-# ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
-#  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
-#  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
-#  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
-#           ░     ░ ░      ░  ░
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stk=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stk:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stk.append(to)
+                            to = next(to)
+                        else:
+                            stk.pop()
+                            if not stk:
+                                break
+                            to = stk[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            print('\033[92m', end='')
+            print(*args, **kwargs)
+            print('\033[0m', end='')
+
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+    class lst_lst:
+        def __init__(self, n) -> None:
+            self.n = n
+            self.pre = []
+            self.cur = []
+            self.lst = [-1] * n
+
+        def append(self, i, j):
+            self.pre.append(self.lst[i])
+            self.lst[i] = len(self.cur)
+            self.cur.append(j)
+
+        def iterate(self, i):
+            tmp = self.lst[i]
+            while tmp != -1:
+                yield self.cur[tmp]
+                tmp = self.pre[tmp]
+'''
+> File Name: Arena.py
+> Author: Wu YiFan
+> Github: https://github.com/wyf0722
+> Mail: wyf0722chosen@gmail.com
+'''
 MULTITEST = 1
 MOD = 1_000_000_007
 def main():
     pass
 
-t = 1
+T = 1
 if MULTITEST:
-    t = II()
-for _ in range(t):
+    T = II()
+for _ in range(T):
     main()
