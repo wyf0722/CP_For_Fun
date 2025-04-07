d9a252ccfb0acc06fe1cc5a9628961db1fe5b000
wyf0722 <522022230116@smail.nju.edu.cn>
2025-04-07 19:41:39 +0800

    feat(CP_nowcoder): round 88 CD

---
+++ CP_nowcoder/2025/106318/D.cpp
@@ -0,0 +1,46 @@
+/*************************************************************************
+    > File Name: 漫步.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-04-07 19:31
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+#define MULTICASE 1
+void solve() {
+    int x;
+    cin >> x;
+    // x是x+z的子集
+    if ((x & (x + 1)) == 0) {
+        cout << -1 << endl;
+    } else {
+        for (int i = 0; i < 31; i++) {
+            if (x >> i & 1) continue;
+            cout << (1 << i) << endl;
+            return;
+        }
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
