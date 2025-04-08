90c54cf6be8288e8ffeeca7a987e74b57f77090f
wyf0722 <522022230116@smail.nju.edu.cn>
2025-04-09 01:37:51 +0800

    feat(CP_codeforces): round1016 ABC

---
+++ CP_codeforces/2025/2093/B.cpp
@@ -0,0 +1,47 @@
+/*************************************************************************
+    > File Name: B_Expensive_Number.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-04-09 01:15
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+#define MULTICASE 1
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    int idx = -1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] != '0') {
+            idx = i;
+            break;
+        }
+    }
+    int pre_zero = 0;
+    for (int i = 0; i < idx; i++) if (s[i] == '0') pre_zero++;
+    cout << n - 1 - pre_zero << "\n";
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
---
+++ CP_codeforces/2025/2093/C.cpp
@@ -0,0 +1,54 @@
+/*************************************************************************
+    > File Name: C_Simple_Repetition.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-04-09 01:21
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+#define MULTICASE 1
+void solve() {
+    int x, k;
+    cin >> x >> k;
+    if (k >= 2 && x >= 2) {
+        cout << "NO" << "\n";
+        return;
+    }
+    int val = x;
+    for (int i = 0; i < k - 1; i++) {
+        val = val * 10 + x;
+    }
+
+    auto is_prime = [](int x) -> bool {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    };
+
+    cout << (is_prime(val) ? "YES" : "NO") << "\n";
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
