ef6b3bfea7bc8b8429fe4c06af8a12e7313d9707
wyf0722 <522022230116@smail.nju.edu.cn>
2025-04-20 12:22:03 +0800

    feat(CP_atcoder): abc402 AB

---
+++ CP_atcoder/2025/abc402/B.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+    > File Name: B_Restaurant_Queue.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-04-19 23:46
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+#define MULTICASE 0
+void solve() {
+    int q;
+    cin >> q;
+    queue<int> line;
+    while (q--) {
+        int op;
+        cin >> op;
+        if (op == 1) {
+            int x;
+            cin >> x;
+            line.push(x);
+        } else {
+            cout << line.front() << endl;
+            line.pop();
+        }
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
