5b927fd5b5498cbdb34c93e40e2669e651220d38
wyf0722 <522022230116@smail.nju.edu.cn>
2025-04-20 12:56:05 +0800

    feat(CP_leetcode): weekly446 ABC

---
+++ CP_leetcode/weekly/440-449/446/A.py
@@ -0,0 +1,69 @@
+import os
+import random
+import sys
+from typing import *
+from collections import defaultdict, Counter, deque
+from functools import cache, reduce
+from itertools import pairwise, combinations, permutations, groupby, accumulate
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+from heapq import *
+from math import gcd, lcm, isqrt
+from operator import add, sub, mul, floordiv, truediv, and_, or_, xor
+
+from types import GeneratorType
+
+
+def bootstrap(f, stack=[]):
+
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+
+    return wrappedfunc
+
+
+input = sys.stdin.readline
+output = lambda x: sys.stdout.write(str(x) + "\n")
+outputL = lambda x: sys.stdout.write(" ".join(map(str, x)) + "\n")
+
+printerr = lambda *args, **kwargs: print(
+    "\u001B[31m", *args, "\u001B[0m", file=sys.stderr, **kwargs)
+
+I = lambda: input().rstrip("\n")
+II = lambda: int(input())
+MII = lambda: map(int, input().split())
+LMII = lambda: list(MII())
+TMII = lambda: tuple(MII())
+LI = lambda: list(I())
+LSI = lambda: list(map(int, I()))
+
+MULTITEST = 1
+class Solution:
+    def calculateScore(self, instructions: List[str], values: List[int]) -> int:
+        n = len(values)
+        vis = [0] * n
+        ans = 0
+        i = 0
+        while 0 <= i < n:
+            op = instructions[i]
+            if vis[i] == 1:
+                break
+            vis[i] = 1
+            if op == 'add':
+                ans += values[i]
+                i += 1
+            else:
+                i += values[i]
+        return ans
