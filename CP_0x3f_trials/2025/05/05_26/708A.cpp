cd20794dd554e4c0716e4e682062c2c06e32fb5a
wyf0722 <522022230116@smail.nju.edu.cn>
2025-05-28 13:33:27 +0800

    feat(CP_0x3f_trials): 708A

---
+++ CP_0x3f_trials/2025/05/05_26/708A.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+    > File Name: A_Letters_Cyclic_Shift.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-05-28 13:29
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+#define MULTICASE 0
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+        if (s[i] != 'a') {
+            for (int j = i; j < n && s[j] != 'a'; j++) {
+                s[j] = s[j] - 1;
+            }
+            cout << s << endl;
+            return;
+        }
+    }
+    // 全a
+    s.back() = 'z';
+    cout << s << endl;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
