2c1cb4c17184306664d455ab51880114ca3bcdd0
wyf0722 <522022230116@smail.nju.edu.cn>
2025-06-03 15:30:59 +0800

    feat(CP_0x3f_trials): 1974C

---
+++ CP_0x3f_trials/2025/06/06_02/1974C.cpp
@@ -0,0 +1,74 @@
+/*************************************************************************
+    > File Name: C_Beautiful_Triple_Pairs.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-06-03 15:25
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+#define MULTICASE 1
+void solve() {
+    int n;
+    cin >> n;
+    V<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    map<tuple<int, int, int>, int> mp;
+    for (int i = 0; i < n - 2; i++) {
+        mp[{a[i], a[i + 1], a[i + 2]}]++;
+    }
+    map<tuple<int, int, int>, int> pre;
+    i64 ans = 0;
+    for (auto &[k, v] : mp) {
+        auto [x, y, z] = k;
+        ans += 1LL * v * pre[{x, y, -1}];
+        ans += 1LL * v * pre[{x, -1, z}];
+        ans += 1LL * v * pre[{-1, y, z}];
+        pre[{x, y, -1}] += v;
+        pre[{x, -1, z}] += v;
+        pre[{-1, y, z}] += v;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int T = 1;
+#if MULTICASE
+    cin >> T;
+#endif
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
