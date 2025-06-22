d1c6ce6b88cd6d7a9fc5f3f667a887d007695327
wyf0722 <522022230116@smail.nju.edu.cn>
2025-06-22 21:45:17 +0800

    feat(CP_leetcode): weekly455 ABC

---
+++ CP_leetcode/weekly/450-459/455/A.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-06-22 21:12
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+class Solution {
+public:
+    bool checkPrimeFrequency(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for (auto &x : nums) {
+            cnt[x]++;
+        }      
+        for (auto &[_, v] : cnt) {
+            if (v == 1) continue;
+            bool is_prime = true;
+            for (int i = 2; i * i <= v; i++) {
+                if (v % i == 0) {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if (is_prime) return true;
+        }
+        return false;
+    }
+};
