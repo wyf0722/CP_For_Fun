b2db1e4db28d01b1919a16edfc6b8d353a1c4ebb
wyf0722 <522022230116@smail.nju.edu.cn>
2025-08-11 00:24:43 +0800

    feat(CP_leetcode): weekly462 A

---
+++ CP_leetcode/weekly/460-469/462/A.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-08-11 00:20
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        for (int i = x; i < x + k / 2; i++) {
+            for (int j = y; j < y + k; j++) {
+                // i + ii = x + x + k - 1
+                // ii = 2 * x + k - 1 - i
+                swap(grid[i][j], grid[2 * x + k - 1 - i][j]);
+            }
+        }
+        return grid;
+    }
+};
