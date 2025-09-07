ddcfb6a080ebb842e5dbc383f3263f1890b0871d
wyf0722 <522022230116@smail.nju.edu.cn>
2025-09-08 00:18:06 +0800

    feat(CP_leetcode): weekly466 ABC

---
+++ CP_leetcode/weekly/460-469/466/B.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+    > File Name: B.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-09-07 23:56
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+class Solution {
+public:
+    int minOperations(string s) {
+        vector<int> c(26);
+        for (auto &ch : s) {
+            c[ch - 'a']++;
+        }
+        int ans = 0;
+        int i = 1;
+        for (; i < 26 && c[i] == 0; i++) {}
+        return 26 - i;
+    }
+};
