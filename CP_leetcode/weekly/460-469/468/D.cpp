279487643f4d7bb01c1015c752a28010c61b2ae0
wyf0722 <522022230116@smail.nju.edu.cn>
2025-09-27 00:11:00 +0800

    feat(CP_leetcode): weekly 468 D

---
+++ CP_leetcode/weekly/460-469/468/D.cpp
@@ -0,0 +1,96 @@
+/*************************************************************************
+    > File Name: D.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-09-26 23:57
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+/**
+ * https://github.com/the-tourist/algo/blob/master/data/sparsetable.cpp
+ */
+template <typename T, typename F>
+class SparseTable {
+   public:
+    int n;
+    vector<vector<T>> mat;
+    F func;
+
+    SparseTable(const vector<T>& a, const F& f) : func(f) {
+        n = static_cast<int>(a.size());
+        int max_log = 32 - __builtin_clz(n);
+        mat.resize(max_log);
+        mat[0] = a;
+        for (int j = 1; j < max_log; j++) {
+            mat[j].resize(n - (1 << j) + 1);
+            for (int i = 0; i <= n - (1 << j); i++) {
+                mat[j][i] = func(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
+            }
+        }
+    }
+
+    T query(int from, int to) const {
+        assert(0 <= from && from <= to && to <= n - 1);
+        int lg = 32 - __builtin_clz(to - from + 1) - 1;
+        return func(mat[lg][from], mat[lg][to - (1 << lg) + 1]);
+    }
+};
+
+auto get_max = [](auto a, auto b) { return max(a, b); };
+
+auto get_min = [](auto a, auto b) { return min(a, b); };
+
+// SparseTable<int, decltype(get_max)> st(input_data, get_max);
+
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n = nums.size();
+        SparseTable<int, decltype(get_max)> st_mx(nums, get_max);
+        SparseTable<int, decltype(get_min)> st_mn(nums, get_min);
+        auto f = [&](int l, int r) {
+            return st_mx.query(l, r) - st_mn.query(l, r);
+        };
+        max_heap<tuple<int, int, int>> h;
+        for (int i = 0; i < n; i++) {
+            h.emplace(f(i, n - 1), i, n - 1);
+        }
+
+        i64 ans = 0;
+        while (k--) {
+            auto [val, l, r] = h.top();
+            h.pop();
+            ans += val;
+            if (r > l) h.emplace(f(l, r - 1), l, r - 1);
+        }
+        return ans;
+    }
+};
