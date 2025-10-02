c5bd8e1ed095ddf051db26a3d33bfb089ec9c020
wyf0722 <522022230116@smail.nju.edu.cn>
2025-10-02 16:40:21 +0800

    feat(CP_leetcode): weekly469 ABC

---
+++ CP_leetcode/weekly/460-469/469/C.cpp
@@ -0,0 +1,66 @@
+/*************************************************************************
+    > File Name: C.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-10-02 16:12
+************************************************************************/
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 1000000007; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+class Solution {
+public:
+    int zigZagArrays(int n, int l, int r) {
+        r = r - l;
+        V<V<int>> dp0(n, V<int>(r + 1));
+        V<V<int>> dp1(n, V<int>(r + 1));
+        for (int i = 0; i <= r; i++) {
+            dp0[0][i] = 1;
+            dp1[0][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            i64 ls = 0;
+            for (int j = 0; j <= r - 1; j++) {
+                ls = (ls + dp1[i - 1][j]) % MOD;
+                dp0[i][j + 1] = ls;
+            }
+            i64 rs = 0;
+            for (int j = r; j >= 1; j--) {
+                rs = (rs + dp0[i - 1][j]) % MOD;
+                dp1[i][j - 1] = rs;
+            }
+        }
+        i64 ans = 0;
+        for (int val = 0; val <= r; val++) {
+            ans = (ans + dp0[n - 1][val] + dp1[n - 1][val]) % MOD;
+        }
+        return ans;
+    }
+};
