0ee5863b3310a257c74ea61131272ec698fd8b26
wyf0722 <522022230116@smail.nju.edu.cn>
2025-10-12 15:34:51 +0800

    feat(CP_leetcode): biweekly 167 C

---
+++ CP_leetcode/biweekly/160-169/167/C.cpp
@@ -0,0 +1,65 @@
+/*************************************************************************
+    > File Name: C.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-10-12 15:08
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+class ExamTracker {
+public:
+    vector<i64> pre_t, pre_s;
+    ExamTracker() {
+        pre_t = {0};
+        pre_s = {0};
+    }
+    
+    void record(int time, int score) {
+        pre_t.push_back(time);
+        pre_s.push_back(pre_s.back() + score);
+    }
+    
+    long long totalScore(int startTime, int endTime) {
+        return preTimeScore(endTime) - preTimeScore(startTime - 1);
+    }
+
+    long long preTimeScore(int t) {
+        int idx = lstTrue(0, (int)pre_t.size() - 1, [&](int i) { return pre_t[i] <= t; });
+        return pre_s[idx];
+    }
+};
+
+/**
+ * Your ExamTracker object will be instantiated and called as such:
+ * ExamTracker* obj = new ExamTracker();
+ * obj->record(time,score);
+ * long long param_2 = obj->totalScore(startTime,endTime);
+ */
