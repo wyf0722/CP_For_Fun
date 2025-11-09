91ed13e59578fd9011b02090355cfd336955e037
wyf0722 <522022230116@smail.nju.edu.cn>
2025-11-10 00:18:07 +0800

    feat(CP_leetcode): biweekly169 ABD

---
+++ CP_leetcode/biweekly/160-169/169/D.cpp
@@ -0,0 +1,127 @@
+/*************************************************************************
+    > File Name: B.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-11-10 00:01
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+template <typename T>
+struct Fenwick {
+    int n;
+    std::vector<T> a;
+
+    Fenwick(int n_ = 0) {
+        init(n_);
+    }
+
+    void init(int n_) {
+        n = n_;
+        a.assign(n, T{});
+    }
+
+    void add(int x, const T &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] = a[i - 1] + v;
+        }
+    }
+
+    T sum(int x) {
+        T ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            ans = ans + a[i - 1];
+        }
+        return ans;
+    }
+
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+
+    int select(const T &k) {
+        int x = 0;
+        T cur{};
+        for (int i = 1 << std::__lg(n); i; i /= 2) {
+            if (x + i <= n && cur + a[x + i - 1] <= k) {
+                x += i;
+                cur = cur + a[x - 1];
+            }
+        }
+        return x;
+    }
+};
+
+template <typename T>
+struct Discrete {
+    Discrete() {}
+    void add(T t) { p.push_back(t); }
+
+    void init() {
+        sort(p.begin(), p.end());
+        p.resize(unique(p.begin(), p.end()) - p.begin());
+    }
+
+    int size() { return p.size(); }
+
+    int query(T t) { return lower_bound(p.begin(), p.end(), t) - p.begin(); }
+
+    T operator[](int id) { return p[id]; }
+
+    vector<T>& get() { return p; }
+    vector<T> p;
+};
+
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        // (pre[j] - pre[i]) * 2 > j - i
+        // 2 * pre[j] - j > 2 * pre[i] - i
+        int n = nums.size();
+        V<i64> pre(n + 1);
+        for (int i = 0; i < n; ++i) {
+            pre[i + 1] = pre[i] + (nums[i] == target ? 1 : 0);
+        }
+        Discrete<i64> discrete;
+        for (int i = 0; i <= n; i++) {
+            discrete.add(2 * pre[i] - i);
+        }
+        discrete.init();
+        i64 ans = 0;
+        Fenwick<int> fen(n + 1);
+        fen.add(discrete.query(0), 1);
+        for (int i = 0; i < n; i++) {
+            int id = discrete.query(2 * pre[i + 1] - (i + 1));
+            ans += fen.rangeSum(0, id);
+            fen.add(id, 1);
+        }
+        return ans;
+    }
+};
