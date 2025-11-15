a38f088e0cf7239fae7752946cd9b71e5150cf2a
wyf0722 <522022230116@smail.nju.edu.cn>
2025-11-15 15:10:12 +0800

    feat(CP_leetcode): weekly475 C

---
+++ CP_leetcode/weekly/470-479/475/C.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+    > File Name: C.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2025-11-15 15:00
+************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+const int inf = INT_MAX / 2 - 100;
+const i64 infLL = LLONG_MAX / 3;
+constexpr int MOD = 998244353; // 998244353 1000000007
+int dx[4]{1, 0, -1, 0}, dy[4]{0, 1, 0, -1};
+template <class T> using V = vector<T>;
+template<typename T> using min_heap=priority_queue<T,vector<T>,greater<T>>;
+template<typename T> using max_heap=priority_queue<T>;
+
+constexpr int popcount(int x) { return __builtin_popcount(x); }
+constexpr int topbit(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); }
+
+template <class T> bool chmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }  // set a = min(a,b)
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }  // set a = max(a,b)
+
+template<class T, class U> T fstTrue(T lo, T hi, U f) { ++hi; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo) / 2; f(mid) ? hi = mid : lo = mid + 1; } return lo; }
+template<class T, class U> T lstTrue(T lo, T hi, U f) { --lo; assert(lo <= hi); while (lo < hi) { T mid = lo + (hi - lo + 1) / 2; f(mid) ? lo = mid : hi = mid - 1; } return lo; }
+
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& g, int K) {
+        int m = g.size(), n = g[0].size();
+        V<V<V<int>>> dp(m, V<V<int>>(n, V<int>(K + 1, -inf)));
+        dp[0][0][0] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int cost = min(g[i][j], 1);
+                for (int k = cost; k <= K; k++) {
+                    if (i) chmax(dp[i][j][k], dp[i - 1][j][k - cost] + g[i][j]);
+                    if (j) chmax(dp[i][j][k], dp[i][j - 1][k - cost] + g[i][j]);
+                }
+            }
+        }
+        int ans = -inf;
+        for (int k = 0; k <= K; k++) {
+            chmax(ans, dp[m - 1][n - 1][k]);
+        }
+        return ans < 0 ? -1 : ans;
+    }
+};
