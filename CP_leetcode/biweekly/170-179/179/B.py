b83d17848f834444fc36b8696c5f61c14d6cd290
wyf0722 <522022230116@smail.nju.edu.cn>
2026-03-30 00:23:22 +0800

    feat(CP_leetcode): biweekly179 AB

---
+++ CP_leetcode/biweekly/170-179/179/B.py
@@ -0,0 +1,240 @@
+standard_input, packages, output_together = 1, 1, 0
+dfs, hashing, read_from_file = 0, 0, 0
+de = 1
+
+if 1:
+
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
+
+        import math
+        inf = math.inf
+
+        def I():
+            return input()
+
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return input().split()
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
+                        break
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
+
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
+
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
+
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
+
+        sys.stdout = IOWrapper(sys.stdout)
+
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stk=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stk:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stk.append(to)
+                            to = next(to)
+                        else:
+                            stk.pop()
+                            if not stk:
+                                break
+                            to = stk[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            print('\033[92m', end='')
+            print(*args, **kwargs)
+            print('\033[0m', end='')
+
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+    class lst_lst:
+        def __init__(self, n) -> None:
+            self.n = n
+            self.pre = []
+            self.cur = []
+            self.lst = [-1] * n
+
+        def append(self, i, j):
+            self.pre.append(self.lst[i])
+            self.lst[i] = len(self.cur)
+            self.cur.append(j)
+
+        def iterate(self, i):
+            tmp = self.lst[i]
+            while tmp != -1:
+                yield self.cur[tmp]
+                tmp = self.pre[tmp]
+'''
+> File Name: B.py
+> Author: Wu YiFan
+> Github: https://github.com/wyf0722
+> Mail: wyf0722chosen@gmail.com
+> Created Time: 2026-03-30 00:20
+'''
+class Combinatorics:
+    def __init__(self, n, mod):
+        assert mod > n
+        self.n = n + 10
+        self.mod = mod
+
+        self.perm = [1]
+        self.rev = [1]
+        self.inv = [0]
+        self.fault = [0]
+
+        self.build_perm()
+        self.build_rev()
+        self.build_inv()
+        self.build_fault()
+        return
+
+    def build_perm(self):
+        self.perm = [1] * (self.n + 1)  # (i!) % mod
+        for i in range(1, self.n + 1):
+            self.perm[i] = self.perm[i - 1] * i % self.mod
+        return
+
+    def build_rev(self):
+        self.rev = [1] * (self.n + 1)  # pow(i!, -1, mod)
+        self.rev[-1] = pow(self.perm[-1], -1, self.mod)  # GcdLike().mod_reverse(self.perm[-1], self.mod)
+        for i in range(self.n - 1, 0, -1):
+            self.rev[i] = (self.rev[i + 1] * (i + 1) % self.mod)  # pow(i!, -1, mod)
+        return
+
+    def build_inv(self):
+        self.inv = [0] * (self.n + 1)  # pow(i, -1, mod)
+        self.inv[1] = 1
+        for i in range(2, self.n + 1):
+            self.inv[i] = (self.mod - self.mod // i) * self.inv[self.mod % i] % self.mod
+        return
+
+    def build_fault(self):
+        self.fault = [0] * (self.n + 1)  # fault permutation
+        self.fault[0] = 1
+        self.fault[2] = 1
+        for i in range(3, self.n + 1):
+            self.fault[i] = (i - 1) * (self.fault[i - 1] + self.fault[i - 2])
+            self.fault[i] %= self.mod
+        return
+
+    def comb(self, a, b):
+        if a < b:
+            return 0
+        res = self.perm[a] * self.rev[b] * self.rev[a - b]  # comb(a, b) % mod = (a!/(b!(a-b)!)) % mod
+        return res % self.mod
+
+    def factorial(self, a):
+        res = self.perm[a]  # (a!) % mod
+        return res % self.mod
+
+    def inverse(self, n):
+        res = self.perm[n - 1] * self.rev[n] % self.mod  # pow(n, -1, mod)
+        return res
+
+    def catalan(self, n):
+        res = (self.comb(2 * n, n) - self.comb(2 * n, n - 1)) % self.mod
+        return res
+
+MOD = 10**9 + 7
+class Solution:
+    def countVisiblePeople(self, n: int, pos: int, k: int) -> int:
+        comb = Combinatorics(n, MOD)
+        return 2 * comb.comb(n - 1, k) % MOD
