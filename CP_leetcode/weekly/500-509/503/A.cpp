5de571479e2c6404aa8dd04c7324f923adbeb258
wyf0722 <522022230116@smail.nju.edu.cn>
2026-05-29 00:38:17 +0800

    feat(CP_leetcode): weekly503 AB

---
+++ CP_leetcode/weekly/500-509/503/A.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2026-05-29 00:30
+************************************************************************/
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for (int x : nums) {
+            if (mp[x] != k) {
+                mp[x]++;
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
