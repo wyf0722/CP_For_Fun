f5fac15df9b2d6ec85c5b66abd921e9009f02591
wyf0722 <522022230116@smail.nju.edu.cn>
2026-06-04 00:33:37 +0800

    feat(CP_leetcode): biweekly170 D

---
+++ CP_leetcode/biweekly/170-179/170/D.py
@@ -0,0 +1,215 @@
+standard_input, packages, output_together = 1, 1, 0
+dfs, hashing, read_from_file = 0, 0, 0
+de = 1
+
+if 1:
+
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
+
+        import math
+        inf = math.inf
+
+        def I():
+            return input()
+
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return input().split()
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
+                        break
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
+
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
+
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
+
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
+
+        sys.stdout = IOWrapper(sys.stdout)
+
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stk=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stk:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stk.append(to)
+                            to = next(to)
+                        else:
+                            stk.pop()
+                            if not stk:
+                                break
+                            to = stk[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            print('\033[92m', end='')
+            print(*args, **kwargs)
+            print('\033[0m', end='')
+
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+    class lst_lst:
+        def __init__(self, n) -> None:
+            self.n = n
+            self.pre = []
+            self.cur = []
+            self.lst = [-1] * n
+
+        def append(self, i, j):
+            self.pre.append(self.lst[i])
+            self.lst[i] = len(self.cur)
+            self.cur.append(j)
+
+        def iterate(self, i):
+            tmp = self.lst[i]
+            while tmp != -1:
+                yield self.cur[tmp]
+                tmp = self.pre[tmp]
+'''
+> File Name: D.py
+> Author: Wu YiFan
+> Github: https://github.com/wyf0722
+> Mail: wyf0722chosen@gmail.com
+> Created Time: 2026-06-04 00:24
+'''
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+
+        def f(x: int) -> int:
+            if x < 100:
+                return 0
+            s = list(map(int, str(x)))
+            n = len(s)
+
+            @lru_cache(maxsize=None)
+            def dp(i: int, c: int, lstCmp: int, lstDig: int, is_num: bool, isLimit: bool) -> int:
+                if i == n:
+                    return c
+                res = 0
+
+                # ====================== 第一步：先处理 not is_num（不填数字）======================
+                if not is_num:
+                    res += dp(i + 1, c, lstCmp, lstDig, False, False)
+
+                # ====================== 第二步：再处理 is_num（填数字）======================
+                low = 1 if not is_num else 0  # 前导零时从 1 开始填，避免重复
+                high = s[i] if isLimit else 9
+                for d in range(low, high + 1):
+                    newLimit = isLimit and (d == high)
+                    new_num = True
+
+                    if not is_num:
+                        # 填第一个数字
+                        res += dp(i + 1, c, 0, d, new_num, newLimit)
+                    else:
+                        # 已有数字，判断趋势
+                        currCmp = 1 if d > lstDig else (-1 if d < lstDig else 0)
+                        add = 0
+                        if lstCmp != 0:
+                            # 形成三位，判断峰/谷
+                            if lstCmp * currCmp == -1:
+                                add = 1
+                        res += dp(i + 1, c + add, currCmp, d, new_num, newLimit)
+
+                return res
+
+            return dp(0, 0, 0, 0, False, True)
+
+        return f(num2) - f(num1 - 1)
