d6ff48242e4f96b023f121d60a6b1a20eb2827bb
wyf0722 <522022230116@smail.nju.edu.cn>
2026-06-19 17:43:43 +0800

    feat(CP_leetcode): weekly505 ABC

---
+++ CP_leetcode/weekly/500-509/505/A.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+    > File Name: A.cpp
+    > Author: Wu YiFan
+    > Github: https://github.com/wyf0722
+    > Mail: wyf0722chosen@gmail.com
+    > Created Time: 2026-06-19 17:11
+************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define debug(...) cout << '[' << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using i64 = long long;
+
+class Solution {
+public:
+    int sumOfGoodIntegers(int n, int k) {
+        int ans = 0;
+        for (int i = max(0, n - k); i <= n + k; i++) {
+            if ((n & i) == 0) {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+};
